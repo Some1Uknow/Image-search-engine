648a5d7292b4de5700baeed985352234203f57f9
Some1Uknow <130275024+Some1Uknow@users.noreply.github.com>
2023-09-08 10:42:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,70 @@
-const colorbtn = document.querySelector(".colorbtn");
-const bodybg = document.querySelector("body");
+document.addEventListener("DOMContentLoaded", () => {
+    const accessKey = "uXWNSf2jTSjOsHGPI1jMKkLVKBwBeFtenWNKc1ParIw"; 
+    const formEl = document.querySelector("form");
+    const inputEl = document.getElementById("searchbar");
+    const searchResults = document.getElementById("searchResults");
+    const showMore = document.getElementById("show-more-button");
+    const submitButton = document.getElementById("submit"); // Get the Search button element
 
-const colors = [
-  "yellow",
-  "red",
-  "green",
-  "blue",
-  "#3b5998",
-  "purple",
-  "orange",
-  "cyan",
-  "magenta",
-  "brown",
-  "pink",
-  "teal",
-  "navy",
-  "violet",
-  "gold",
-  "silver",
-  "crimson",
-  "indigo",
-  "lime",
-  "salmon",
-  "aquamarine",
-  "peru",
-  "plum",
-  "tomato",
-  "olive",
-  "chocolate",
-  "seagreen",
-  "slateblue",
-];
+    let inputData = "";
+    let page = 1;
 
-colorbtn.addEventListener("click", changeColor);
+    async function searchImages() {
+      inputData = inputEl.value;
+      const url = `https://api.unsplash.com/search/photos?query=${inputData}&per_page=3&page=${page}`;
 
-function changeColor() {
-  let random = Math.floor(Math.random() * colors.length);
-  bodybg.style.backgroundColor = colors[random];
-}
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Client-ID ${accessKey}`
+        }
+      });
+
+      const data = await response.json();
+      const results = data.results;
+
+      if (page === 1) {
+        searchResults.innerHTML = "";
+      }
+
+      results.forEach(result => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description || inputData;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description || inputData;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+      });
+
+      page++;
+      toggleShowMoreButton(results.length > 0);
+    }
+
+    function toggleShowMoreButton(show) {
+      showMore.style.display = show ? "block" : "none";
+    }
+
+    formEl.addEventListener("submit", event => {
+      event.preventDefault();
+      page = 1;
+      searchImages();
+    });
+
+    showMore.addEventListener("click", event => {
+      event.preventDefault();
+      searchImages();
+    });
+
+    submitButton.addEventListener("click", () => {
+      page = 1;
+      searchImages();
+    });
+
+    searchImages();
+  });
